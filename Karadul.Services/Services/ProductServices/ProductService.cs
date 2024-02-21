3cadf7dd5146badc12df0a50ec7f7d037566d7d4
MusabTurk <musabahmetturk@gmail.com>
2024-02-21 17:29:24 +0300

    BaseEntity oluşturduk ve Product entitymizi BaseEntityden türettik. Migration işlemi yaptık ver Product tablomuzu Database e kayıt ettik. Data katmanında Generic Repository ve Product Repositoryleri oluşturduk ve CRUD işlemleri yaptık. Service katmanında ProductService oluşturduk ve CRUD işlemleri yaptık.

---
+++ Karadul.Services/Services/ProductServices/ProductService.cs
@@ -0,0 +1,45 @@
+﻿using Karadul.Data.Entities;
+using Karadul.Data.Repository.ProductRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Karadul.Services.Services.ProductServices
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<Product> CreateAsync(Product product)
+        {
+            return await _productRepository.CreateAsync(product);
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            return await _productRepository.DeleteAsync(id);
+        }
+
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            return await _productRepository.GetAllAsync();
+        }
+
+        public async Task<Product> GetByIdAsync(int id)
+        {
+            return await _productRepository.GetByIdAsync(id);
+        }
+
+        public async Task<Product> UpdateAsync(Product product)
+        {
+            return await _productRepository.UpdateAsync(product);
+        }
+    }
+}
